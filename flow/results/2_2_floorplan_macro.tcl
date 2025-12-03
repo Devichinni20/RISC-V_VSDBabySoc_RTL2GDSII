527b08dcecc7373e3bc1fdc068cb8020a376318f
Devichinni20 <chinnidevi1024@gmail.com>
2025-12-03 17:05:21 +0530

    Upload files

---
+++ flow/results/2_2_floorplan_macro.tcl
@@ -0,0 +1,3 @@
+place_macro -macro_name {dac} -location {100.225 100.105} -orientation R0
+place_macro -macro_name {pll} -location {1700.01 100} -orientation R0
+
---
+++ flow/results/3_2_place_iop.tcl
@@ -0,0 +1,11 @@
+#Edge: BOTTOM
+place_pin -pin_name ENb_CP -layer met2 -location {1723.39 0.242} -force_to_die_boundary
+place_pin -pin_name ENb_VCO -layer met2 -location {1714.19 0.242} -force_to_die_boundary
+place_pin -pin_name OUT -layer met2 -location {1136.43 0.242} -force_to_die_boundary
+place_pin -pin_name REF -layer met2 -location {1707.75 0.242} -force_to_die_boundary
+place_pin -pin_name VCO_IN -layer met2 -location {1734.43 0.242} -force_to_die_boundary
+#Edge: RIGHT
+#Edge: TOP
+#Edge: LEFT
+place_pin -pin_name VREFH -layer met3 -location {0.4 659.94} -force_to_die_boundary
+place_pin -pin_name reset -layer met3 -location {0.4 921.06} -force_to_die_boundary
